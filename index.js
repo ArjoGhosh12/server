5c9cb58d05df6604d8cbf03e65d713d84860feb2
Arjo Ghosh <122384138+ArjoGhosh12@users.noreply.github.com>
2023-11-27 22:23:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 const express = require("express");
-const https = require("https");
 const http = require("http");
-const fs = require("fs");
 const cors = require("cors");
 const { Server } = require("socket.io");
 
 const app = express();
 app.use(cors());
 
-const httpServer = http.createServer(app);
-
-// For testing purposes, generate a self-signed certificate
-const credentials = {
-  key: fs.readFileSync("server-key.pem"),
-  cert: fs.readFileSync("server-cert.pem"),
-};
-
-const httpsServer = https.createServer(credentials, app);
-
-const io = new Server(httpsServer, {
+const server = http.createServer(app);
+const io = new Server(server, {
   cors: {
     origin: "https://client-chatapp.vercel.app/",
     methods: ["GET", "POST"],
@@ -42,14 +31,7 @@ io.on("connection", (socket) => {
   });
 });
 
-// Choose the appropriate port for HTTP and HTTPS
-const httpPort = process.env.HTTP_PORT || 3000;
-const httpsPort = process.env.HTTPS_PORT || 3001;
-
-httpServer.listen(httpPort, () => {
-  console.log(`HTTP Server running on http://localhost:${httpPort}`);
-});
-
-httpsServer.listen(httpsPort, () => {
-  console.log(`HTTPS Server running on https://localhost:${httpsPort}`);
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
